76398a396cf3d7124b4cadb5fde832cbf4193220
Dmitrii <dmitrii@Dmitriis-MacBook-Air.local>
2025-01-20 16:53:23 +0100

    Implemented get_application_by_id function

--- app/crud/application.py
+++ app/crud/application.py
@@ -1,11 +1,14 @@
 from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import application as ApplicationDBModel
 
-async def get_application_by_id(application_id: int, db_session: AsyncSession = None) -> ApplicationDBModel:
-
-    return 
+async def get_application_by_id(application_id: int, db_session: AsyncSession = None):
+    application = (await db_session.scalars(select(ApplicationDBModel).where(ApplicationDBModel.id == application_id))).first()
+    if not application:
+        raise HTTPException(status_code=404, detail=f"Application {application_id} not found")
+    return application
 
 async def place_application(db_session: AsyncSession = None) -> None:
     print("Writing to DB: ")
--- app/models/application.py
+++ app/models/application.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 class ApplicationDBModel(Base):
     __tablename__ = "application"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True)
-    username: Mapped[str] = mapped_column(index=True) 
-    description: Mapped[str] = mapped_column(index=True)
-    created_at: datetime
+    username: Mapped[str] = mapped_column(String, index=True) 
+    description: Mapped[str] = mapped_column(Text, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now())
