b8e6aff3567582c2d6ee0735abfde3282a10f5b2
Dmitrii <dmitrii@Dmitriis-MacBook-Air.local>
2025-01-21 17:09:08 +0100

    Moved logic on running application from main to __init__.py to simplify access for testing.

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from api.routers.application import router as application_router
+from app.config import config
+from app.database import sessionmanager
+
+def init_app(init_db=True) -> FastAPI:
+    lifespan = None
+    if init_db:
+        sessionmanager.init(config.DB_CONFIG)
+
+        # TODO: what is this for?
+        @asynccontextmanager
+        async def lifespan(app: FastAPI):
+            yield
+            if sessionmamanger.engine:
+                await sessionmanager.close()
+
+    app = FastAPI(title="BeWise HomeAssignment", lifespan=lifespan)
+
+    @app.get("/")
+    async def root():
+        return {"message": "BeWise HomeAssignment"}
+
+    app.include_router(application_router)
+    return app
--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
 import uvicorn
 
-from fastapi import FastAPI
+from . import init_app
 
-from api.routers.application import router as application_router
-
-app = FastAPI(title="BeWise HomeAssignment")
-
-@app.get("/")
-async def root():
-    return {"message": "BeWise HomeAssignment"}
-
-app.include_router(application_router)
+app = init_app()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
