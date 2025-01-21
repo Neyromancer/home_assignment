2064a439cce486b59ec0e3bafb7181a9a09a79c3
Dmitrii <dmitrii@Dmitriis-MacBook-Air.local>
2025-01-21 17:11:26 +0100

    Added DB config

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    # TODO: How to set up local variables for `getenv`?
+    DB_CONFIG = os.getenv(
+        "DB_CONFIG",
+        "postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}".format(
+            DB_USER=os.getenv("DB_USER", "fastapi"),
+            DB_PASSWORD=os.getenv("DB_PASSWORD", "fastapi-password"),
+            DB_HOST=os.getenv("DB_HOST", "fastapi-postgresql:5432"),
+            DB_NAME=os.getenv("DB_NAME", "fastapi"),
+        )
+    )
+
+config = Config
