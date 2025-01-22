6cdec52cebe43a5fb3b04ee8a65de4eebf2f225a
Dmitrii <dmitrii@Dmitriis-MacBook-Air.local>
2025-01-22 16:51:33 +0100

    app/crud/application.py with app/crud/database_requests.py to improve readability

--- app/api/routers/application.py
+++ app/api/routers/application.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud import application
+from app.crud import database_requests
 from app.schemas.application import ApplicationBase, ApplicationResponse, ApplicationCreate
 from app.models.application import ApplicationDBModel
 from app.database import get_db_session
@@ -25,18 +25,18 @@ db_id: list[int] = [2]
 # TODO: Check what is the best practice for filtering
 @router.get("/{username}", response_model=list[ApplicationResponse])
 async def application_details(username: str, db_session: AsyncSession = Depends(get_db_session)) -> list[ApplicationResponse]:
-    applications = await application.fetch_by_username(db_session, username)
-    return list(map(ApplicationResponse.from_orm, applications))
+    fetched_user_applications = await database_requests.fetch_by_username(db_session, username)
+    return list(map(ApplicationResponse.from_orm, fetched_user_applications))
 
 
 @router.get("/", response_model=list[ApplicationResponse])
 async def get_applications(page: int = Query(1, ge=1, description="Page number"), size: int = Query(10, ge=1, le=100, description="Items per page"), db_session: AsyncSession = Depends(get_db_session)) -> list[ApplicationResponse]:
     # Why is this strange formula ` skip=(page - 1) * size` used?
-    applications = await application.fetch_all(db_session, skip=(page - 1) * size, limit=size)
-    return [ApplicationResponse.model_validate(application) for application in applications]
+    fetched_applications = await database_requests.fetch_all(db_session, skip=(page - 1) * size, limit=size)
+    return [ApplicationResponse.model_validate(application) for application in fetched_applications]
 
 
 @router.post("/", response_model=ApplicationResponse)
-async def create_application(application: ApplicationCreate, db_session: AsyncSession = Depends(get_db_session)) -> ApplicationResponse:
-    application = await application.create(db_session, application)
-    return ApplicationResponse.from_orm(application)
+async def create_application(application_to_save: ApplicationCreate, db_session: AsyncSession = Depends(get_db_session)) -> ApplicationResponse:
+    database_saved_application = await database_requests.create(db_session, application_to_save)
+    return ApplicationResponse.from_orm(database_saved_application)
